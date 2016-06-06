f5654dcbb1df950a197949424f8087b53301a323
Steven Van Rossem <steven.vanrossem@intec.ugent.be>
2016-06-06 17:09:00 +0200

    update DD connection

---
+++ NFs/docker/elastic-router/ctrl_app/start_DD2.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#This gives time to the UN to configure the network namespace of the container
+sleep 3
+
+#DD command
+#./ryu_ddclient.py -k ./DoubleDecker-master/keys/a-keys.json -d tcp://172.17.0.1:5555 ryu a &
+#./ryu_ddclient.py -k ./DoubleDecker-py-master/doubledecker/a-keys.json -d tcp://172.17.0.1:5555 ryu a &
+#./ryu_ddclient.py -k ./a-keys.json -d tcp://172.17.0.1:5555 ryu a &
+
+echo "Control app container started"
+
+#echo "start ssh"
+#service ssh start
+
+echo "start ryu"
+cd ryu_app/
+ryu-manager ctrl_app_er_un_zmq_v3.py
+
+# keep container running
+#while true
+#do
+#	sleep 1
+#done
+
