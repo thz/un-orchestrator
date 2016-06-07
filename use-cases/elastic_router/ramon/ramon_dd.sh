fc4bd249008adc8a4871ef34aebb041644c58ac5
Unify Durak <unify@durak.testbed.se>
2016-06-07 19:14:41 +0200

    Environment variables for RAMON parameters. /PD

--- use-cases/elastic_router/ramon/ramon_dd.sh
+++ use-cases/elastic_router/ramon/ramon_dd.sh
@@ -9,8 +9,18 @@ NO_HELLO=--no_hello             # use this when debugging
 #NO_HELLO=''                     # use this in "production"
 
 RATEMON_CLIENT=./ratemon_client.py
-RAMON_PATH=../../../../aggregator/ramon/ramon_svn/run_monitor.py
+## Temporary path to RAMON
+#RAMON_PATH=../../../../aggregator/ramon/ramon_svn/run_monitor.py
+## Use this path when the submodule for RAMON is OK.
+RAMON_PATH=./ramon_src/run_monitor.py
 
-#xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k /etc/doubledecker/a-keys.json ${name} a -p ${ramon_port} -q ${config_port} --ramon_args -i ${interface} -s 20 -e 1 -k 10 -m 1 -q ${config_port}; read" &
+DOUBLEDECKER_KEYS=/etc/doubledecker/public-keys.json
 
-xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k /etc/doubledecker/public-keys.json ${name} a ${NO_HELLO} -p ${ramon_port} -q ${config_port}  --ramon_path ${RAMON_PATH} --ramon_args -i ${interface} -s 20 -e 1 -k 10 -m 1 -a 90 -o 90 -q ${config_port}; read" &
+RAMON_SAMPLE_RATE=20
+RAMON_ESTIMATION_INTERVAL=1
+RAMON_LINK_SPEED=10
+RAMON_METER_INTERVAL=1
+RAMON_ALARM_TRIGGER=90
+RAMON_CUTOFF=90
+
+xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k ${DOUBLEDECKER_KEYS} ${name} a ${NO_HELLO} -p ${ramon_port} -q ${config_port} --ramon_path ${RAMON_PATH} --ramon_args -i ${interface} -s ${RAMON_SAMPLE_RATE} -e ${RAMON_ESTIMATION_INTERVAL} -k ${RAMON_LINK_SPEED} -m ${RAMON_METER_INTERVAL} -a ${RAMON_ALARM_TRIGGER} -o ${RAMON_CUTOFF} -q ${config_port}; read" &
