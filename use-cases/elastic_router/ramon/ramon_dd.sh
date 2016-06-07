b989dede340e05c3ffa119487866db3ca8d0aa19
Unify Durak <unify@durak.testbed.se>
2016-06-07 17:59:07 +0200

    Change from a-keys to public-keys for RAMON. /PD

--- use-cases/elastic_router/ramon/ramon_dd.sh
+++ use-cases/elastic_router/ramon/ramon_dd.sh
@@ -13,4 +13,4 @@ RAMON_PATH=../../../../aggregator/ramon/ramon_svn/run_monitor.py
 
 #xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k /etc/doubledecker/a-keys.json ${name} a -p ${ramon_port} -q ${config_port} --ramon_args -i ${interface} -s 20 -e 1 -k 10 -m 1 -q ${config_port}; read" &
 
-xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k /etc/doubledecker/a-keys.json ${name} a ${NO_HELLO} -p ${ramon_port} -q ${config_port}  --ramon_path ${RAMON_PATH} --ramon_args -i ${interface} -s 20 -e 1 -k 10 -m 1 -a 90 -o 90 -q ${config_port}; read" &
+xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k /etc/doubledecker/public-keys.json ${name} a ${NO_HELLO} -p ${ramon_port} -q ${config_port}  --ramon_path ${RAMON_PATH} --ramon_args -i ${interface} -s 20 -e 1 -k 10 -m 1 -a 90 -o 90 -q ${config_port}; read" &
