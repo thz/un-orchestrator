5032304543ff4a954635a9e7f9bdaac3e399fd27
Bertrand Pechenot <berpec@acreo.se>
2016-06-08 17:05:46 +0200

    Updated ramon container

--- use-cases/elastic_router/ramon/ramon_dd.sh
+++ use-cases/elastic_router/ramon/ramon_dd.sh
@@ -16,11 +16,29 @@ RAMON_PATH=./ramon_src/run_monitor.py
 
 DOUBLEDECKER_KEYS=/etc/doubledecker/public-keys.json
 
-RAMON_SAMPLE_RATE=20
-RAMON_ESTIMATION_INTERVAL=1
-RAMON_LINK_SPEED=10
-RAMON_METER_INTERVAL=1
-RAMON_ALARM_TRIGGER=90
-RAMON_CUTOFF=90
+RAMON_SAMPLE_RATE=20    # samples/second; 20 means sample every 50ms
+RAMON_ESTIMATION_INTERVAL=1 # estimate the distribution every 1 second
+RAMON_LINK_SPEED=10     # link speed of the interface monitored, in Mbits/second
+RAMON_METER_INTERVAL=1  # report (to ratemon_client) every 1 second
+RAMON_ALARM_TRIGGER=90  # over this risk (in percent) the monitor will
+                        # trigger an overload alarm
+RAMON_CUTOFF=90         # use this percentage as a cutoff when calculating
+                        # the overload risk
 
-xterm -T ${name}_dd -e "${RATEMON_CLIENT} -k ${DOUBLEDECKER_KEYS} ${name} a ${NO_HELLO} -p ${ramon_port} -q ${config_port} --ramon_path ${RAMON_PATH} --ramon_args -i ${interface} -s ${RAMON_SAMPLE_RATE} -e ${RAMON_ESTIMATION_INTERVAL} -k ${RAMON_LINK_SPEED} -m ${RAMON_METER_INTERVAL} -a ${RAMON_ALARM_TRIGGER} -o ${RAMON_CUTOFF} -q ${config_port}; read" &
+xterm -T ${name}_dd \
+        -e "${RATEMON_CLIENT} \
+        -k ${DOUBLEDECKER_KEYS} \
+        ${name} \
+        ${NO_HELLO} \
+        -p ${ramon_port} \
+        -q ${config_port} \
+        --ramon_path ${RAMON_PATH} \
+        --ramon_args \
+        -i ${interface} \
+        -s ${RAMON_SAMPLE_RATE} \
+        -e ${RAMON_ESTIMATION_INTERVAL} \
+        -k ${RAMON_LINK_SPEED} \
+        -m ${RAMON_METER_INTERVAL} \
+        -a ${RAMON_ALARM_TRIGGER} \
+        -o ${RAMON_CUTOFF} \
+        -q ${config_port}; read" &
